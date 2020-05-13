1a46767ae4058fe7b2e72c5a95cefc383e67d446
R2 <r2@vivobook.lan>
2020-05-13 10:41:39 -0500

    Import export

--- graph_generate/export.py
+++ graph_generate/export.py
@@ -2,11 +2,24 @@ import os
 import sys
 import json
 import sys
+import numpy as np
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 from graph.Graph import Graph
+from algorithms.floyd_warshall import *
+
+def export_matrix(matrix_result):
+
+    matrix_to_export = []
+    for row in matrix_result:
+        matrix_to_export.append([
+            str(a) if a == np.inf else str(int(a))
+            for a in row
+        ])
+
+    return matrix_to_export
 
 num_nodes = 100
 probability_edges = 0.1
@@ -23,11 +36,17 @@ else:
 
 
 graph = Graph.creategraph(num_nodes, probability_edges)
+dist = Floyd_Warshall(graph)
 graph.insert_random_edge(weights=[1])
+
+graph_values = {
+    "graph": graph.export_values(),
+    "dist": export_matrix(dist)
+}
+
+print("-------------------------")
 print("Filename: " + filename)
 graph.stats()
-
-graph_values = graph.export_values()
 
 with open(filename, 'w') as outfile:
     json.dump(graph_values, outfile)
--- graph_generate/import.py
+++ graph_generate/import.py
@@ -16,9 +16,14 @@ else:
     print("Faltan argumentos")
     exit()
 
+print("Filename: ", filename)
 data_import = {}
 with open(filename) as json_file:
     data_import = json.load(json_file)
 
 graph = Graph.import_values(data_import)
+
+graph.insert_random_edge()
+
+
 graph.stats()
